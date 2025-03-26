ac732be81421a244b91ce6205a809a069196e135
Rachmat Rizki <rahmatrizki089@gmail.com>
2025-03-26 16:28:53 +0700

    chore: setup

---
+++ sprint-app/src/main/java/com/ucok/sprint_app/SprintAppApplication.java
@@ -0,0 +1,13 @@
+package com.ucok.sprint_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintAppApplication.class, args);
+	}
+
+}
